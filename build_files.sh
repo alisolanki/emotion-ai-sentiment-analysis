54cc6bcbd828b6837844d927786c3b26d7430c4b
Ali Solanki <alisolanki123@gmail.com>
2023-02-13 01:43:09 +0530

    venv 👀

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo " BUILD STARTED "
+python3.9 -m venv env
+source env/bin/activate
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD ENDED "
