0f01c128b1c11b86c1f0de714d632e50204b575a
Ali Solanki <alisolanki123@gmail.com>
2023-02-13 18:28:18 +0530

    railway deploy👀

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'sentiment_analysis.wsgi'
--- build_files.sh
+++
@@ -1,4 +0,0 @@
-echo " BUILD STARTED "
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD ENDED "
