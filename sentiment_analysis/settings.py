e87328e9c3654b8ae591eed36278a35c8117b724
Ali Solanki <alisolanki123@gmail.com>
2023-02-13 18:56:47 +0530

    vercel + railway i guess 👀

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -55,7 +55,8 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'https://alisolanki.com/',
     'https://emotion-ai.alisolanki.com/',
-    'https://emotion-ai-sentiment-analysis-production.up.railway.app/'
+    'https://emotion-ai-sentiment-analysis-production.up.railway.app/',
+    'https://emotion-ai-sentiment-analysis-7p6hk7mo2-alisolanki.vercel.app/',
 ]
 
 ROOT_URLCONF = 'sentiment_analysis.urls'
